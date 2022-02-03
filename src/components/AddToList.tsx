3da1c409f094190e4dc2fb3f465f7d9f464e76db
Chris <info@chrisgkalfas.com>
2022-02-04 00:01:53 +0200

    minor changes

--- src/components/AddToList.tsx
+++ src/components/AddToList.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
+import { IState as Props } from "../App";
 
-const AddToList = () => {
+interface IProps {
+  people: Props["people"];
+  setPeople: React.Dispatch<React.SetStateAction<Props["people"]>>;
+}
+
+const AddToList: React.FC<IProps> = () => {
   const [input, setInput] = useState({
     name: "",
     age: "",
