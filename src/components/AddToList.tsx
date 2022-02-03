e0727168226f298012592fe8cc2ee082de157d16
Chris <info@chrisgkalfas.com>
2022-02-03 23:38:17 +0200

    minor changes

--- src/components/AddToList.tsx
+++ src/components/AddToList.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddToList = () => {
-  return <div>AddToList Component</div>;
+  const [input, setInput] = useState({
+    name: "",
+    age: "",
+    note: "",
+    img: "",
+  });
+
+  return (
+    <div className="AddToList">
+      <input
+        type="text"
+        placeholder="name"
+        className="AddToList-input"
+        value={input.name}
+      />
+      <input
+        type="text"
+        placeholder="Age"
+        className="AddToList-input"
+        value={input.age}
+      />
+      <input
+        type="text"
+        placeholder="Image Url"
+        className="AddToList-input"
+        value={input.img}
+      />
+      <textarea
+        placeholder="Notes"
+        className="AddToList-input"
+        value={input.note}
+      />
+    </div>
+  );
 };
 
 export default AddToList;
